d2a06896a3301319089108fc1bc58d6dd9b78b68
ChangappaMA <mukkatira.changappa@gmail.com>
2022-03-25 00:19:06 +0530

    Querying and parameter

--- FastApp/main.py
+++ FastApp/main.py
@@ -1,12 +1,24 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {"data": {"name": "Chang"}}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {"data": f'{limit} published blogs from the database'}
+    return {"data": "All the blocks"}
 
-@app.get('/about')
-def about():
-    return {"data": {"about": "Fastapi App"}}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'All Unpublished Blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1','2'}}
