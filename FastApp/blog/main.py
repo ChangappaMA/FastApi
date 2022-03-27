312ce7a1dd1848c6c757cab7d845e6498013bad1
ChangappaMA <mukkatira.changappa@gmail.com>
2022-03-27 15:57:26 +0530

    remaining CRUD methods

--- FastApp/blog/main.py
+++ FastApp/blog/main.py
@@ -1,5 +1,5 @@
-from typing import final
-from fastapi import FastAPI, Depends
+from urllib import request
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from .schemas import Blog
 from .models import Base, BlogModel
 from .database import SessionLocal, engine
@@ -16,10 +16,47 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create_blog(blog: Blog, db: Session = Depends(get_db)):
     new_blog = BlogModel(title=blog.title, body=blog.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id: int, db: Session = Depends(get_db)):
+    blog = db.query(BlogModel).filter(BlogModel.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return "Done"
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id: int, blog: Blog, db: Session = Depends(get_db)):
+    blogg = db.query(BlogModel).filter(BlogModel.id == id)
+    if not blogg.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blogg.update(blog.dict())
+    db.commit()
+    return 'Updated'
+
+@app.get('/blog')
+def get_all_blog(db: Session = Depends(get_db)):
+    blogs = db.query(BlogModel).all()
+    return blogs
+
+
+@app.get('/blog/{id}', status_code=200)
+def get_a_blog(id: int, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(BlogModel).filter(BlogModel.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+    return blog
+
+
+#### 2:08:02 ######
